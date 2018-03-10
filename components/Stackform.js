21ee9080aa948987966d929bbeda46fa66b9859f
RafBro8 <rromn8@gmail.com>
2018-03-10 13:24:10 -0600

    added addStack functionality + completes the app

--- components/Stackform.js
+++ components/Stackform.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import { addStack } from "../actions/index";
 
 class Stackform extends Component {
     constructor() {
@@ -20,8 +22,23 @@ class Stackform extends Component {
         this.setState({ cards });
     }
 
+    updateCardPart(event, index, part) {
+        const { cards } = this.state;
+
+        cards[index][part] = event.target.value;
+
+        this.setState({ cards });
+    }
+
+    addStack(){
+        console.log('Stackform state', this.state);
+        this.props.addStack(this.state);
+    }
+
     render() {
+        //console.log('Stackform state', this.state);
         return(
+
             <div>
                 <Link to='/' className='link-home'>
                     <h4>Home</h4>
@@ -32,14 +49,37 @@ class Stackform extends Component {
                     <FormGroup>
                         <ControlLabel>Title:</ControlLabel>
                         {' '}
-                        <FormControl />
+                        <FormControl onChange={event => this.setState({ title: event.target.value })}/>
                     </FormGroup>
+                    {
+                        this.state.cards.map((card, index) => {
+                            return (
+                                <div key={card.id}>
+                                    <br/>
+                                    <FormGroup>
+                                        <ControlLabel>Prompt:</ControlLabel>
+                                        {' '}
+                                        <FormControl onChange={event => this.updateCardPart(event, index, 'prompt')}
+                                        />
+                                        {' '}
+                                        <ControlLabel>Answer:</ControlLabel>
+                                        {' '}
+                                        <FormControl
+                                            onChange={event => this.updateCardPart(event, index, 'answer')}
+                                        />
+                                    </FormGroup>
+                                </div>
+                            )
+                        })
+                    }
                 </Form>
                 <br />
                 <Button onClick={() => this.addCard()}>Add Card</Button>
+                {' '}
+                <Button onClick={() => this.addStack()}>Save and Add Stack</Button>
             </div>
         )
     }
 }
 
-export default Stackform;
+export default connect(null, { addStack }) (Stackform);
