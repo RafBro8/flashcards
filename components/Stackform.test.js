03e47d6557b960f630df2ec4f9c9ed9d6fb4de22
RafBro8 <rromn8@gmail.com>
2018-03-11 16:43:20 -0500

    added more tests for components

--- components/Stackform.test.js
+++ components/Stackform.test.js
@@ -24,4 +24,60 @@ describe('Stackform', () => {
     it('renders a button to submit the Form', () => {
         expect(stackForm.find('Button').at(1).props().children).toEqual('Save and Add Stack');
     });
+
+    describe('and updating the title', () => {
+        beforeEach(() => {
+          stackForm.find('FormControl').simulate('change', { target: {value: 'change title' }})
+        });
+
+        it('updates the title in state', () => {
+           //console.log(stackForm.state());
+           expect(stackForm.state().title).toEqual('change title');
+        });
+    });
+
+    describe('when adding a new card', () => {
+        beforeEach(() => {
+            stackForm.find('Button').at(0).simulate('click');
+        });
+
+
+        afterEach(() => {
+            stackForm.setState({ cards: []});
+
+
+        });
+        it('adds a new card to the state', () => {
+            expect(stackForm.state().cards.length).toEqual(1);
+        });
+
+        it('renders the prompt section', () => {
+            expect(stackForm.find('ControlLabel').at(1).props().children).toEqual('Prompt:');
+        });
+
+        it('renders the answer section', () => {
+            expect(stackForm.find('ControlLabel').at(2).props().children).toEqual('Answer:');
+        });
+
+        describe('and updating the card prompt ', () => {
+            beforeEach(() => {
+                stackForm.find('FormControl').at(1).simulate('change', { target: {value: 'change prompt' } });
+            });
+
+            it('updates the prompt in the state', () => {
+                //console.log(stackForm.state());
+                expect(stackForm.state().cards[0].prompt).toEqual('change prompt');
+            });
+        });
+
+        describe('and updating the card answer', () => {
+            beforeEach(() => {
+                stackForm.find("FormControl").at(2).simulate('change', {target: { value: 'change answer' } });
+            });
+
+            it('updates the asnwer in the state', () => {
+                expect(stackForm.state().cards[0].answer).toEqual('change answer');
+            });
+        });
+    });
 });
